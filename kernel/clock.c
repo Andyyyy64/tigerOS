151b3c671104d95b24412d7a18d3f536d7c30285
andy <105579829+Andyyyy64@users.noreply.github.com>
2026-02-19 02:32:23 +0900

    [openTiger] [Rework] Enable timer interrupt and periodic tick logging (#62)
    Task ID: afac7ea2-a4d8-44b7-9d7a-4c6c663f5b0c
    
    Changes:
    - arch/riscv/timer.c
    - include/riscv_timer.h
    - kernel/clock.c
    - kernel/trap.c

--- kernel/clock.c
+++ kernel/clock.c
@@ -7,24 +7,14 @@
 enum {
   CLOCK_INTERVAL_TICKS = 1000000ULL,
   CLOCK_LOG_LIMIT = 4U,
-  SIE_STIE = 1ULL << 5,
-  SSTATUS_SIE = 1ULL << 1,
 };
 
 static volatile uint64_t g_tick_count;
 static uint64_t g_next_deadline;
 static uint32_t g_tick_log_count;
 
-static inline void csr_set_sie(uint64_t mask) {
-  __asm__ volatile("csrs sie, %0" : : "r"(mask));
-}
-
-static inline void csr_set_sstatus(uint64_t mask) {
-  __asm__ volatile("csrs sstatus, %0" : : "r"(mask));
-}
-
 static void clock_program_deadline(void) {
-  uint64_t now = riscv_timer_now();
+  uint64_t now = riscv_timer_read_time();
 
   if (g_next_deadline <= now) {
     uint64_t missed = ((now - g_next_deadline) / CLOCK_INTERVAL_TICKS) + 1ULL;
@@ -37,11 +27,10 @@ static void clock_program_deadline(void) {
 void clock_init(void) {
   g_tick_count = 0ULL;
   g_tick_log_count = 0U;
-  g_next_deadline = riscv_timer_now() + CLOCK_INTERVAL_TICKS;
+  g_next_deadline = riscv_timer_read_time() + CLOCK_INTERVAL_TICKS;
 
   riscv_timer_set_deadline(g_next_deadline);
-  csr_set_sie(SIE_STIE);
-  csr_set_sstatus(SSTATUS_SIE);
+  riscv_timer_enable_interrupts();
 }
 
 void clock_handle_timer_interrupt(void) {
