c739166bc638e55d84b45123b54b6ca044c41ddc
andy <takuyarossi77@gmail.com>
2026-02-19 01:27:24 +0900

    [openTiger] Implement 4KiB physical page allocator
    Task ID: d273c089-3bef-45f8-8020-f68b50f80fdc
    
    Changes:
    - Makefile
    - kernel/main.c
    - include/mm_init.h
    - include/page_alloc.h
    - kernel/mm/init.c
    - kernel/mm/page_alloc.c
    - kernel/mm/page_alloc_test.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,12 @@
 #include "console.h"
 #include "line_io.h"
+#include "mm_init.h"
 
 void kernel_main(void) {
   char line[128];
 
   console_init();
+  mm_init();
   line_io_write("BOOT: kernel entry\n");
   line_io_write("console: line io ready\n");
 
